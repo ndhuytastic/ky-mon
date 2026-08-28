2441666e067cf8d5c3964a9fc67951322d98a478
ndhuytastic <ndhuy.amazing@gmail.com>
2026-08-28 10:46:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -378,7 +378,7 @@ def qimen_analyzer_hojo(cung_data, can_tuan, p_land):
             rank = FORMATION_RANKS.get(raw_name)
             if rank == 3: continue 
             if rank: 
-                display_name = f"{raw_name}<span style='font-size: 0.8em; font-weight: normal; color: #666; margin-top: 2px; writing-mode: horizontal-tb; text-combine-upright: all; -webkit-text-combine: horizontal;'>({rank})</span>"
+                display_name = f"{raw_name}<span style='font-size: 0.85em; font-weight: normal; color: #666; margin-top: 2px; writing-mode: horizontal-tb; text-combine-upright: all; -webkit-text-combine: horizontal;'>({rank})</span>"
             else: 
                 display_name = raw_name
             formatted_list.append((display_name, color))
